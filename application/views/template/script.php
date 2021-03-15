8146a834db7e38a085d684955096bb183ba3903f
DedyHrmawan <dedyhermawan076@gmail.com>
2021-03-15 10:41:06 +0700

    assets

---
+++ application/views/template/script.php
@@ -0,0 +1,3 @@
+<script src="<?= base_url(); ?>assets/js/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/scripts.js"></script>
