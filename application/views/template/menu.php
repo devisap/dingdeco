23481abeeed30ffb7402dd82dee7af7db2f59854
DedyHrmawan <dedyhermawan076@gmail.com>
2021-03-15 10:40:29 +0700

    Library template

---
+++ application/views/template/menu.php
@@ -0,0 +1,82 @@
+<div class="nav accordion" id="accordionSidenav">
+    <a class="nav-link " href="<?php echo site_url('Dashboard'); ?>">
+        <i class="fas fa-home ml-2 mr-3 fa-lg"></i>
+        Dashboard
+    </a>
+    <a class="nav-link " href="javascript:void(0);">
+        <i class="fas fa-users ml-2 mr-3 fa-lg"></i>
+        Daftar Klien
+    </a>
+    <a class="nav-link" href="javascript:void(0);">
+        <i class="fas fa-dollar-sign ml-2 mr-3 fa-lg"></i>
+        Alur Keuangan
+    </a>
+    <a class="nav-link collapsed" href="javascript:void(0);" data-toggle="collapse" data-target="#collapseDashboards" aria-expanded="false" aria-controls="collapseDashboards">
+        <i class="fas fa-project-diagram ml-2 mr-3 fa-lg"></i>
+        Proyek
+        <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+    </a>
+    <div class="collapse" id="collapseDashboards" data-parent="#accordionSidenav">
+        <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-list ml-2 mr-2 fa-lg"></i>
+                Daftar Pemesanan
+            </a>
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-envelope ml-2 mr-2 fa-lg"></i>
+                Surat Kontrak Kerja
+            </a>
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-file-invoice-dollar ml-2 mr-2 fa-lg"></i>
+                Nota Pembayaran
+            </a>
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-truck-moving ml-2 mr-2 fa-lg"></i>
+                Nota Pengiriman
+            </a>
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-map-marked-alt ml-2 mr-2 fa-lg"></i>
+                SOP
+            </a>
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-sort-numeric-up ml-2 mr-2 fa-lg"></i>
+                Pemasukan
+            </a>
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-sort-numeric-down ml-2 mr-2 fa-lg"></i>
+                Pengeluaran
+            </a>
+        </nav>
+    </div>
+    <a class="nav-link" href="javascript:void(0);" data-toggle="collapse" data-target="#collapseHaji" aria-expanded="false" aria-controls="collapseHaji">
+        <i class="fas fa-warehouse ml-2 mr-3 fa-lg"></i>
+        Inventaris Barang
+    </a>
+    <a class="nav-link collapsed" href="javascript:void(0);" data-toggle="collapse" data-target="#collapseWisataHalal" aria-expanded="false" aria-controls="collapseWisataHalal">
+        <i class="fas fa-clipboard ml-2 mr-3 fa-lg"></i>
+        Laporan
+        <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+    </a>
+    <div class="collapse" id="collapseWisataHalal" data-parent="#accordionSidenav">
+        <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-money-bill-wave ml-2 mr-2 fa-lg"></i>
+                Keuangan
+            </a>
+            <a class="nav-link" href="javascript:void(0);">
+                <i class="fas fa-route ml-2  mr-2 fa-lg"></i>
+                Akhir Acara
+            </a>
+        </nav>
+    </div>
+    <a class="nav-link " href="javascript:void(0);">
+        <i class="fa fa-newspaper ml-2 mr-3 fa-lg"></i>
+        Paket
+    </a>
+</div>
+<div class="sidenav-footer">
+    <div class="sidenav-footer-content">
+        <div class="sidenav-footer-subtitle">Login Sebagai:</div>
+        <div class="sidenav-footer-title">Admin</div>
+    </div>
+</div>
