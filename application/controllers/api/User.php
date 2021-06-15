7aa0c42d5d3f90533ef9d74d49ba1305acde4e8a
DedyHrmawan <dedyhermawan076@gmail.com>
2021-06-15 22:45:26 +0700

    API Login

---
+++ application/controllers/api/User.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class User extends RestController
+{
+
+    function __construct()
+    {
+        // Construct the parent class
+        parent::__construct();
+    }
+
+    public function login_post()
+    {
+        $param = $this->post();
+        if (!empty($param['username']) && !empty($param['password'])) {
+            $user = $this->db->get_where('PENGGUNA', ['NAMA_PENGGUNA' => $param['username']])->result();
+            if ($user != null) {
+                $resLogin = $this->db->get_where(
+                    'PENGGUNA',
+                    ['NAMA_PENGGUNA' => $param['username'], 'PASSWORD_PENGGUNA' => $param['password']]
+                )->result();
+                if ($resLogin != null) {
+                    $this->response(['status' => true, 'message' => 'Data berhasil ditemukan' , 'data' => $resLogin[0]], 200);
+                }else{
+                    $this->response(['status' => false, 'message' => 'Username atau password salah' ], 404);
+                }
+            } else {
+                $this->response(['status' => false, 'message' => 'Data tidak ditemukan'], 404);
+            }
+        } else {
+            $this->response(['status' => false, 'message' => 'Parameter tidak cocok'], 404);
+        }
+    }
+}
